27162cc968288f6cdedd4fd75b2d66a787ad1b56
winterSteve25 <w1nter.ish3re@gmail.com>
2024-06-27 12:20:57 -0400

    GC Setup

--- os/configuration.nix
+++ os/configuration.nix
@@ -13,6 +13,7 @@
   config = {
 		# Bootloader.
 		boot.loader.systemd-boot.enable = true;
+		boot.loader.systemd-boot.configurationLimit = 5;
 		boot.loader.efi.canTouchEfiVariables = true;
 
 		# Set your time zone.
@@ -27,14 +28,19 @@
 			packages = with pkgs; [];
 		};
 
-	  nixpkgs.config.allowUnfree = true;
-	  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	  	nixpkgs.config.allowUnfree = true;
+	  	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+		nix.gc = {
+			automatic = true;
+			dates = "weekly";
+			options = "--delete older-than 7d";
+		};
 
-	  environment.systemPackages = with pkgs; [
-		 	vim
-		 	yazi
-		 	git
-	  ];
+		environment.systemPackages = with pkgs; [
+			vim
+			yazi
+			git
+		];
 
 		# Env Vars
 		environment.sessionVariables = {
@@ -42,35 +48,35 @@
 			NIXOS_OZONE_WL = "1";
 		};
 
-	  sound.enable = true;
-	  security.rtkit.enable = true;
-	  services.pipewire = {
+	  	sound.enable = true;
+	  	security.rtkit.enable = true;
+	  	services.pipewire = {
 			enable = true;
 			alsa.enable = true;
 			alsa.support32Bit = true;
 			pulse.enable = true;
 			jack.enable = true;
-	  };
+	  	};
 
-	  xdg.portal.enable = true;
-	  xdg.portal.extraPortals = [
+		xdg.portal.enable = true;
+		xdg.portal.extraPortals = [
 			pkgs.xdg-desktop-portal-gtk
 			pkgs.xdg-desktop-portal-hyprland
-	  ];
+		];
 	  
-	  programs.hyprland = {
+		programs.hyprland = {
 			enable = true;
 			xwayland.enable = true;
-	  };
+		};
 
 		hardware.opengl.enable = true;
 
-	  # This value determines the NixOS release from which the default
-	  # settings for stateful data, like file locations and database versions
-	  # on your system were taken. It‘s perfectly fine and recommended to leave
-	  # this value at the release version of the first install of this system.
-	  # Before changing this value read the documentation for this option
-	  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	  system.stateVersion = "24.05"; # Did you read the comment?
-  };
+		# This value determines the NixOS release from which the default
+		# settings for stateful data, like file locations and database versions
+		# on your system were taken. It‘s perfectly fine and recommended to leave
+		# this value at the release version of the first install of this system.
+		# Before changing this value read the documentation for this option
+		# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	  	system.stateVersion = "24.05"; # Did you read the comment?
+  	};
 }
